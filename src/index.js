bb52186f2feadf354153071aa4ee4ea8cb8843fa
Rakesh Mahendran <rakeshmahendran99@gmail.com>
2022-11-29 00:40:33 +0530

    recent commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+
+import styles from '../styles/Home.module.css'
+import Link from "next/link";
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./_app";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  rootElement
+);
+
+
+
